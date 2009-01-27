8fd211870b220f00a51367993240fb7b58dbf1fc
overv161 <overv161@6cc9ea44-fe55-0410-99c8-6f4f49b7b99b>
2009-01-27 16:15:04 +0000

    Added screen when clients get disconnected from the server, so the chance of them coming back is higher :)

---
+++ beta2/lua/plugins/cl_crashscreen.lua
@@ -0,0 +1,43 @@
+//This file produces a white screen on the clients when the server disconnects without notice
+
+if SERVER then
+	function Ping( ply )
+		umsg.Start( "PingMsg", ply )
+			umsg.Long( os.time() )
+		umsg.End()
+	end
+	concommand.Add( "PingServer", Ping )
+end
+
+if CLIENT then
+	local PrevTime = -1
+	local LastTime = 0
+	local Disconnected = false
+	
+	function PingServer()
+		if PrevTime == LastTime then
+			Disconnected = true
+		else
+			Disconnected = false
+		end
+		PrevTime = LastTime
+	
+		RunConsoleCommand( "PingServer" )
+	end
+	timer.Create( "Server Pinger", 3, 0, PingServer )
+	
+	function ReceivePing( um )
+		LastTime = um:ReadLong()
+	end
+	usermessage.Hook( "PingMsg", ReceivePing )
+	
+	function DrawMessage()
+		if Disconnected then
+			surface.SetDrawColor( Color( 255, 255, 255 ) )
+			surface.DrawRect( 0, 0, ScrW(), ScrH() )
+			draw.SimpleText( "You have been disconnected from the server!", "Trebuchet22", ScrW() / 2, ScrH() / 2, Color(121, 121, 121, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+			draw.SimpleText( "Just disconnect and reconnect as soon as the server is back :)", "Default", ScrW() / 2, (ScrH() / 2) + 15, Color(121, 121, 121, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+		end
+	end
+	hook.Add( "HUDPaint", "DrawMessage", DrawMessage )
+end
