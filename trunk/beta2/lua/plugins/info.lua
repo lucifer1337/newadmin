7c2cdb264d9a7257eadbe57091b4a5bfa886592b
overv161 <overv161@6cc9ea44-fe55-0410-99c8-6f4f49b7b99b>
2009-02-14 12:38:16 +0000

    Fucked the revision number again :V

--- trunk/beta2/lua/plugins/info.lua
+++ trunk/beta2/lua/plugins/info.lua
@@ -67,6 +67,6 @@ RegisterCommand( "List Commands", "List all the commands in the console", "comma
 
 //NewAdmin version
 function Info( ply, params )
-	Notify( "This server is running NewAdmin 1.0 R74", "NOTIFY_GENERIC", ply )
+	Notify( "This server is running NewAdmin 1.0 R76", "NOTIFY_GENERIC", ply )
 end
 RegisterCommand( "Info", "Shows info about NewAdmin, such as the version running", "info", "info", 0, "Overv", 8, 0, Info )
