0ddb354cbbc8c1abcbf428beacf356d4308cfeaa
overv161 <overv161@6cc9ea44-fe55-0410-99c8-6f4f49b7b99b>
2009-02-26 18:49:22 +0000

    You can now turn off the anti spam for ranks. (Default admins and respected)

--- trunk/beta2/lua/plugins/antispam.lua
+++ trunk/beta2/lua/plugins/antispam.lua
@@ -1,16 +1,17 @@
 local MaxRepeats = 10
 
 function AntiPropSpam( ply, mdl )
-	if mdl == ply:GetNWString( "LastProp" ) then
-		if ply:GetNWInt("Repeats") < MaxRepeats then
-			ply:SetNWInt( "Repeats", ply:GetNWInt("Repeats") + 1 )
-			Log( ply:Nick() .. " repeated a prop " .. ply:GetNWInt("Repeats") .. " times now." )
+	if !HasPrivilege( ply, "No AntiSpam" ) then
+		if mdl == ply:GetNWString( "LastProp" ) then
+			if ply:GetNWInt("Repeats") < MaxRepeats then
+				ply:SetNWInt( "Repeats", ply:GetNWInt("Repeats") + 1 )
+			else
+				return false
+			end
 		else
-			return false
-		end
-	else
-		ply:SetNWInt( "Repeats", 0 )
-	end	
-	ply:SetNWString( "LastProp", mdl )
+			ply:SetNWInt( "Repeats", 0 )
+		end	
+		ply:SetNWString( "LastProp", mdl )
+	end
 end
 hook.Add( "PlayerSpawnProp", "AntiPropSpam", AntiPropSpam ) 
